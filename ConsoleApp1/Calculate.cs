1328dd72f4c3a775ad03cc59a4e23da3de747ced
Arthur <developerhakart@yahoo.com>
2020-01-25 11:06:24 +0400

    added comments and make some corrections

--- ConsoleApp1/Calculate.cs
+++ ConsoleApp1/Calculate.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    //if all functions are static make class also static 
+    // rename to Calculator, make noun
     class Calculate
     {
         public static List<string> result = new List<string>();
+        
+        //use complete meaning of methods, instead of Calc use Calculator
         public static List<string> Calc(List<string[]> input) {
+            // change meaningfull variables, change first and second_value
             float first_value_1;
          
             float second_value_1;
@@ -19,6 +24,8 @@ namespace ConsoleApp1
            
             for (int i = 0; i < input.Count-1; i++)
             {
+                // you can remove these variables and direct use into string
+                // sample is bellow
                 first_value_1 = float.Parse(input[i].First());
               //  first_value_2 = float.Parse(input[i+1].First());
                 second_value_1 = float.Parse(input[i].Last());
@@ -26,19 +33,21 @@ namespace ConsoleApp1
                 step = (second_value_2 - second_value_1) / 10;
                 for (int j = 0; j < 10; j++)
                 {
-                  result.Add((first_value_1 + j).ToString() + "\t" + (second_value_1 + j * step).ToString());
+                    // use $ symbol 
+                    // for example $"{first_value_1 + j}\t{second_value_1 + j * step}"
+                    result.Add((first_value_1 + j).ToString() + "\t" + (second_value_1 + j * step).ToString());
                 }
             }
 
+            //same here 
             result.Add(input[input.Count-1].First() + "\t"+ input[input.Count - 1].Last());
             Console.WriteLine();
            
             foreach (var item in result)
-            {
-               
+            {               
                 Console.WriteLine(item);
-
             }
+
             return result;
         }
     }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {
+        {           
             Input_Output.Read_File();
             Input_Output.Write_File(Calculate.Calc(Input_Output.file_lines));
             Search.Search_In_Result();
